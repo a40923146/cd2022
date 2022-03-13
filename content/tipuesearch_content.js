fe043e865fdf61cc0d2e56893e7570ad87439466
a40923146 <a035401763@gmail.com>
2022-03-13 09:03:04 +0800

    4472 HW17 Design Fundamentals

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n', 'tags': '', 'url': 'About.html'}, {'title': 'HW1', 'text': '\n \n \n \n \n   \n \n \n \n \n \n \n', 'tags': '', 'url': 'HW1.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n', 'tags': '', 'url': 'About.html'}, {'title': 'HW1', 'text': '\n \n \n \n \n   \n \n \n \n \n \n \n \n \n \n \n \n \n   \n \n \n \n \n \n \n', 'tags': '', 'url': 'HW1.html'}]};
